4c524a22caa78cfe5d077d6461f1e88b2c90438d
diegosouza007 <dsouza@yahoo.com>
2022-02-13 21:56:04 -0300

    main modal created

---
+++ src/components/MainModal/MainModal.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PlayButton from '../PlayButton/PlayButton'
+import './MainModal.css'
+
+export default function MainModal() {
+  return (
+    <div className="intro">
+        <div>
+            <h1>TIC TAC TOE</h1>
+            <PlayButton />
+        </div>
+    </div>
+  )
+}
